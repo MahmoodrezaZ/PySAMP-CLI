f2ce1bfa85746bc37ea4cf0edbaf08a43b605d7f
MahmoodrezaZ <mahmoodrezazolghadrian@gmail.com>
2024-04-01 12:23:30 +0330

    feat(models/file.py): Added path BaseFile

--- pysamp_cli/models/file.py
+++ pysamp_cli/models/file.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from typing import Optional
 from furl import furl
+from pathlib import Path
 
 
 class BaseFile(ABC):
@@ -13,11 +14,13 @@ class BaseFile(ABC):
             *,
             url: str,
             name: Optional[str] = None,
-            extension: Optional[str] = None
+            extension: Optional[str] = None,
+            path: str | Path = None
     ):
         self.url = furl(url)
         self.name = name
         self.extension = extension
+        self.path = path
 
     @property
     def name(self):
@@ -30,11 +33,11 @@ class BaseFile(ABC):
 
         else:
             self._name = self.url.path.segments[-1]
-            
+
     @property
     def extension(self):
         return self._extension
-    
+
     @extension.setter
     def extension(self, value):
         if value:
@@ -42,3 +45,15 @@ class BaseFile(ABC):
 
         else:
             self._extension = self.name.split('.')[-1]
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, value: str | Path):
+        if isinstance(value, str):
+            self._path = Path(value)
+
+        elif isinstance(value, Path):
+            self._path = value
